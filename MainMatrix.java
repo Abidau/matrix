63440a4c3e0acd9e0a37b9f3cc24b0d70ec8ac06
NaufalAbidAurizky <naufalabidau@gmail.com>
2024-12-07 14:18:25 +0700

    Penambahan Perkalian dan Transpose

--- MainMatrix.java
+++ MainMatrix.java
@@ -67,7 +67,63 @@ public class MainMatrix {
     }
     
     static void Perkalian(double matrix1[][], double matrix2[][]){
-        System.out.println("kali");
+       
+        System.out.println("Masukkan angka matrix 1");
+        for (int i = 0; i < baris1; i++) {
+            for (int j = 0; j < kolom1; j++) {
+                System.out.print("matrix 1 ["+ i +"]["+ j +"] : ");
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println();
+    
+        System.out.println("Masukkan angka matrix 2");
+        for (int i = 0; i < baris2; i++) {
+            for (int j = 0; j < kolom2; j++) {
+                System.out.print("matrix 2 [" + i + "][" + j + "] : ");
+                matrix2[i][j] = sc.nextInt();
+            }
+        } 
+        System.out.println();
+
+        if (kolom1 == baris2) {
+            hasil = new double[baris1][kolom2];
+            for (int i = 0; i < baris1; i++) {
+                for (int j = 0; j < kolom2; j++) {
+                    hasil[i][j] = 0;
+                    for (int k = 0; k < kolom1; k++) {
+                        hasil[i][j] += matrix1[i][k] * matrix2[k][j];
+                    }
+                }
+            }
+        } else {
+            System.out.println("Operasi tidak valid untuk ukuran matrix yang diberikan.");
+        }
+
+        System.out.println("Matrix 1 : ");
+        for (int i = 0; i < baris1; i++) {
+            for (int j = 0; j < kolom1; j++) {
+                System.out.print(matrix1[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Matrix 2 : ");
+        for (int i = 0; i < baris2; i++) {
+            for (int j = 0; j < kolom2; j++) {
+                System.out.print(matrix2[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Hasil Perkalian : ");
+        for (int i = 0; i < baris1; i++) {
+            for (int j = 0; j < kolom2; j++) {
+                System.out.print(hasil[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
     
     static void Invers(double matrix1[][]){
@@ -156,6 +212,42 @@ public class MainMatrix {
     }
     
     static void Transpose(double matrix1[][]){
-        System.out.println("transpos");
+        
+        for (int i = 0; i < baris1; i++) {
+            for (int j = 0; j < kolom1; j++) {
+                System.out.print("matrix 1 ["+ i +"]["+ j +"] : ");
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println();
+
+        double[][] transpose = new double[kolom1][baris1];
+        for (int i = 0; i < baris1; i++) {
+            for (int j = 0; j < kolom1; j++) {
+                transpose[j][i] = matrix1[i][j];
+            }
+        }
+
+        System.out.println("Matrix ");
+        for (int i = 0; i < baris1; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < kolom1; j++) {
+                System.out.print(matrix1[i][j] + " ");
+            }
+            System.out.print("| ");
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.println("Hasil Transpose : ");
+        for (int i = 0; i < kolom1; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < baris1; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.print("| ");
+            System.out.println();
+        }
     }
 }
+
